c31ecd8e9d4f9b25adb5f14c0b39026c2bc5897a
jangir-rishbh <rishbhlamdiwal@gmail.com>
2026-04-01 20:27:35 +0530

    error solv kiya

--- mail.js
+++ mail.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || "587"),
-  secure: process.env.SMTP_PORT === "465",
+  secure: process.env.SMTP_PORT === "465", // true for port 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
